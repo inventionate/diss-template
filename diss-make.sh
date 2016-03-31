be7253271f779673ecdaae238e562c04bfc9486f
inventionate <f.mundt@inventionate.de>
2016-03-31 14:37:25 +0200

    Added image rotation.

--- diss-make.sh
+++ diss-make.sh
@@ -7,7 +7,23 @@
 pandoc -s -S --bibliography bibliography.bib --biblatex --filter $HOME/.cabal/bin/pandoc-crossref index.md -M cref=TRUE -o zeit-raum-studium.tex --latex-engine=lualatex --template=diss-template
 
 # Bilder fixieren
-sed -i '' 's/begin{figure}\[htbp\]/begin{figure}\[H\]/g' 'zeit-raum-studium.tex'
+sed -i '' 's/begin{figure}\[htbp\]/begin{figure}\[H\]/g' zeit-raum-studium.tex
+
+# Bilder ggf. drehen
+sed -i '' '/rotate-start/ {
+    N
+    N
+    s/rotate-start\n.*\n.*/begin{sidewaysfigure}[H]/g
+    }' zeit-raum-studium.tex
+
+sed -i '' '/end{figure}/ {
+    N
+    N
+    /\rotate-end/ {
+        N
+        s/end{figure}\n.*\rotate-end/end{sidewaysfigure}/g
+    }
+}' zeit-raum-studium.tex
 
 # Tabellen nicht trimmern
 # sed -i '' 's/@{}//g' 'zeit-raum-studium.tex'
