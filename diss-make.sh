6ae2d9abb6175282ec9f633f33ed0c15db6198e7
inventionate <f.mundt@inventionate.de>
2016-03-31 12:27:36 +0200

    Files.

---
+++ diss-make.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Dissertation setzen
+# Copyright 2016 Fabian Mundt
+
+# Pandoc ausführen
+pandoc -s -S --bibliography bibliography.bib --biblatex --filter $HOME/.cabal/bin/pandoc-crossref index.md -M cref=TRUE -o zeit-raum-studium.tex --latex-engine=lualatex --template=diss-template
+
+# Bilder fixieren
+sed -i '' 's/begin{figure}\[htbp\]/begin{figure}\[H\]/g' 'zeit-raum-studium.tex'
+
+# Tabellen nicht trimmern
+# sed -i '' 's/@{}//g' 'zeit-raum-studium.tex'
+
+# Überprüfen, ob der gesamte Prozess ausgeführt werden soll (-a)
+while getopts ":a" opt; do
+  case $opt in
+    a)
+        # LuaLaTeX ausführen
+        lualatex zeit-raum-studium.tex
+
+        # Zitate setzen
+        biber zeit-raum-studium.bcf
+
+        # Index erstellen
+        makeglossaries zeit-raum-studium
+
+        # Index erstellen
+        splitindex -M texindy zeit-raum-studium
+
+        # Finales Dokument setzen (zweima, um die Verzeichnisse zu generieren)
+        lualatex zeit-raum-studium.tex
+        lualatex zeit-raum-studium.tex
+        ;;
+  esac
+done
+
+lualatex zeit-raum-studium.tex
